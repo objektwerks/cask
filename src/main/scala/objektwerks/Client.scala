05f270cc95ebd6b0a27e4e727553ede99893779c
objektwerks <objektwerks@runbox.com>
2021-12-22 07:45:58 -0500

    refactored client

--- src/main/scala/objektwerks/Client.scala
+++ src/main/scala/objektwerks/Client.scala
@@ -7,8 +7,11 @@ import upickle.default.*
 
 object Client:
   def main(args: Array[String]): Unit =
-    val register = Register(email = "live@live.com")
-    println(s"Register: $register")
-    val response = requests.post("http://localhost:7272/command", data = write[Register](register))
+    val register = Register("live@live.com")
+    println(s"*** Register: $register")
+    val registerJson = write[Register](register)
+    println(s"*** Register json: $registerJson")
+    val response = requests.post("http://localhost:7272/command", data = registerJson)
+    println(s"*** Response: $response")
     val registered = read[Registered](response.text())
-    println(s"Registered: $registered")
+    println(s"*** Registered: $registered")
