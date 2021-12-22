2f9e0e9e76efb46634798c5096a782497b74f3fd
objektwerks <objektwerks@runbox.com>
2021-12-22 07:48:56 -0500

    edited client

--- src/main/scala/objektwerks/Client.scala
+++ src/main/scala/objektwerks/Client.scala
@@ -9,9 +9,12 @@ object Client:
   def main(args: Array[String]): Unit =
     val register = Register("live@live.com")
     println(s"*** Register: $register")
+
     val registerJson = write[Register](register)
     println(s"*** Register json: $registerJson")
+
     val response = requests.post("http://localhost:7272/command", data = registerJson)
     println(s"*** Response: $response")
+    
     val registered = read[Registered](response.text())
     println(s"*** Registered: $registered")
