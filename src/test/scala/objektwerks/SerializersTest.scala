7a0b74bef8b8e530bb4f43b2611379971204a499
objektwerks <objektwerks@runbox.com>
2021-12-23 02:53:06 -0500

    removed serializer test

--- src/test/scala/objektwerks/SerializersTest.scala
+++
@@ -1,8 +0,0 @@
-package objektwerks
-
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
-
-class SerializersTest extends AnyFunSuite with Matchers:
-  test("command") {
-  }
