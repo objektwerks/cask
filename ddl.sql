7db408acd9fc7a63649e656b9aa6b3b50bbe3f11
objektwerks <objektwerks@runbox.com>
2021-12-31 13:47:08 -0500

    edited ddl.sql email license to account reference

--- ddl.sql
+++ ddl.sql
@@ -114,7 +114,7 @@ CREATE TABLE repair (
 
 CREATE TABLE email (
   id VARCHAR PRIMARY KEY,
-  license VARCHAR(36) NOT NULL,
+  license VARCHAR(36) REFERENCES account(license),
   address VARCHAR NOT NULL,
   message VARCHAR NOT NULL,
   date_sent INT NOT NULL,
