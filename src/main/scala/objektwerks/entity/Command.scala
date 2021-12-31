f9c522ee5c9d30d34f85cbbd3bcd535fd8b3e5ed
objektwerks <objektwerks@runbox.com>
2021-12-31 05:58:19 -0500

    edited register and login regarding email address

--- src/main/scala/objektwerks/entity/Command.scala
+++ src/main/scala/objektwerks/entity/Command.scala
@@ -7,8 +7,8 @@ sealed trait License:
 
 sealed trait Command
 
-final case class Register(email: String) extends Command
-final case class Login(email: String, pin: String) extends Command
+final case class Register(emailAddress: String) extends Command
+final case class Login(emailAddress: String, pin: String) extends Command
 
 final case class Deactivate(license: String) extends Command with License
 final case class Reactivate(license: String) extends Command with License
