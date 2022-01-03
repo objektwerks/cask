7e0f1161f225d86e670449588ed69355e6a59e68
objektwerks <objektwerks@runbox.com>
2022-01-03 09:12:27 -0500

    added web router; refactored web server

--- src/main/scala/objektwerks/WebServer.scala
+++ src/main/scala/objektwerks/WebServer.scala
@@ -1,16 +1,36 @@
 package objektwerks
 
+import cask.main.Main
 import cask.main.MainRoutes
+import com.typesafe.scalalogging.LazyLogging
 
-import scalatags.Text.all.*
+import io.undertow.Undertow
 
-object WebServer extends MainRoutes:
-  @cask.get("/")
-  def hello() =
-    html(
-      body(
-        h1("Hello, Cask and Scalatags!")
-      )
-    ).render
+import objektwerks.router.WebRouter
 
-  initialize()
+import scala.io.StdIn
+
+object WebServer extends Main with LazyLogging:
+  val allRoutes = Seq(WebRouter())
+
+  override def port: Int = 7070
+  override def host: String = "localhost"
+
+  override def main(args: Array[String]): Unit =
+    if (!verbose) Main.silenceJboss()
+    val server = Undertow.builder
+      .addHttpListener(port, host)
+      .setHandler(defaultHandler)
+      .build
+
+    server.start()
+    val started = s"*** Web Server started at http://$host:$port/\nPress RETURN to stop..."
+    println(started)
+    logger.info(started)
+
+    StdIn.readLine()
+    server.stop()
+    val stopped = s"*** Web Server stopped!"
+    println(stopped)
+    logger.info(stopped)
+
---
+++ src/main/scala/objektwerks/router/WebRouter.scala
@@ -0,0 +1,16 @@
+package objektwerks.router
+
+import cask.main.Routes
+
+import scalatags.Text.all.*
+
+case class WebRouter() extends Routes:
+  @cask.get("/")
+  def hello() =
+    html(
+      body(
+        h1("Hello, Cask and Scalatags!")
+      )
+    ).render
+
+  initialize()  
