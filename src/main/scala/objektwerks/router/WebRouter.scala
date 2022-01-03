309bb7a0c7efc79d0dfdfd3896511e09e324435c
objektwerks <objektwerks@runbox.com>
2022-01-03 11:38:26 -0500

    added scaladoc comment to web router for bug #232

--- src/main/scala/objektwerks/router/WebRouter.scala
+++ src/main/scala/objektwerks/router/WebRouter.scala
@@ -4,6 +4,11 @@ import cask.main.Routes
 
 import scalatags.Text.all.*
 
+/**
+ * See Refined Text Tags [ 232 ]
+ * https://github.com/com-lihaoyi/scalatags/commit/47f8f7fbe0d865c6a25aecbf924b41b061829a97
+ * for missing doctype("html")( ... )
+*/
 case class WebRouter() extends Routes:
   @cask.get("/")
   def hello() =
