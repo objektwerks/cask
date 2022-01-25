250ec305f6059dd3011471995f35b18db2cca2f8
objektwerks <objektwerks@runbox.com>
2022-01-25 14:43:32 -0500

    wrapped web router response in cask response

--- src/main/scala/objektwerks/router/WebRouter.scala
+++ src/main/scala/objektwerks/router/WebRouter.scala
@@ -7,12 +7,13 @@ import scalatags.Text.all.*
 case class WebRouter() extends Routes:
   @cask.get("/scalatags")
   def scalatags() =
-    doctype("html") (
+    val page = doctype("html") (
       html(
         body(
           h1("Hello, Cask and Scalatags!")
         )
       )
     ).render
+    cask.Response(page, 200, Seq("Content-Type" -> "text/html; charset=UTF-8"))
 
   initialize()
