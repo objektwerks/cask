bf9300f93d9c76df9cb830d6d0a58ce29d356394
objektwerks <objektwerks@runbox.com>
2022-01-28 05:17:52 -0500

    added resource server

---
+++ src/main/scala/objektwerks/ResourceServer.scala
@@ -0,0 +1,36 @@
+package objektwerks
+
+import cask.main.Main
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+
+import io.undertow.Undertow
+
+import scala.io.StdIn
+
+import objektwerks.router.ResourceRouter
+
+object ResourceServer extends Main with LazyLogging:
+  val allRoutes = Seq(ResourceRouter())
+
+  override def port: Int = 7171
+
+  override def host: String = "localhost"
+
+  override def main(args: Array[String]): Unit =
+    if (!verbose) Main.silenceJboss()
+    val server = Undertow.builder
+      .addHttpListener(port, host)
+      .setHandler(defaultHandler)
+      .build
+
+    server.start()
+    val started = s"*** Server started at http://$host:$port/\nPress RETURN to stop..."
+    println(started)
+    logger.info(started)
+
+    StdIn.readLine()
+    server.stop()
+    val stopped = s"*** Server stopped!"
+    println(stopped)
+    logger.info(stopped)
