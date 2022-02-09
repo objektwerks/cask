31a0fdcd040bf947cb9e954aec8d28fb6f2a6a85
objektwerks <objektwerks@runbox.com>
2022-02-09 12:29:21 -0500

    renamed resources cache to resources with cache method

--- src/main/scala/objektwerks/router/ResourceRouter.scala
+++ src/main/scala/objektwerks/router/ResourceRouter.scala
@@ -14,7 +14,7 @@ class ResourceRouter()
   with Resources(
     basePath = "/public/",
     indexHtml = "index.html",
-    cache = ResourcesCache(minSize = 4, maxSize = 10, expireAfter = 24.hour)):
+    cache = Resources.cache(minSize = 4, maxSize = 10, expireAfter = 24.hour)):
   @cask.get("/")
   def index() = Response(loadResource(indexHtml), 200, Seq(htmlHeader))
 
